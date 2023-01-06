7542b4d64cf81111ab778cda708ac98fb77cd4a6
takenoto <mendesrenan5@gmail.com>
2023-01-06 13:57:32 -0300

    added max_leaf_nodes variance

--- app/random_forest/random_forest.py
+++ app/random_forest/random_forest.py
@@ -12,29 +12,33 @@ from domain.data_dealing.data_split_test_train import DataSplitTestTrain, split_
 def basic_random_forest_loop(X, y):
     
     # A conclusão a princípio é que depende muito mais da qtde e qualidade dos dados do que qualquer outra coisa
-    for min_samples_leaf in range(1, 3):
-        for i_test_size in range(1, 4):
-            for i in range(0, 7):
-                # Parameters
-                test_size=0  + i_test_size/10
-                n_estimators = 50 + i*50
-                
-                # Model creation
-                model = RandomForestRegressor(random_state=0, n_estimators=n_estimators, min_samples_leaf=min_samples_leaf)
-                
-                # Fitting
-                data = split_data_for_test(X, y, test_size=test_size)
-                model.fit(data.train.X, data.train.y)
-                
-                # Testing
-                predictions = model.predict(data.test.X)
-                acc_total = r2_score(predictions, data.test.y)
-                
-                # Debug & Printing
-                print('----------')
-                print(f'nº {i} | test_size={test_size} | n_estimators = {n_estimators} | min_samples_leaf = {min_samples_leaf}')
-                print(f'acc = {acc_total}')
-                # Cuidado que esse aqui é EM VALORES ABSOLUTOS!
-                mean_error = mean_squared_error(predictions, data.test.y)
-                print(f'mean error² = {mean_error}')
-                pass
+    for max_leaf_nodes in range(6, 12):
+        for min_samples_leaf in range(1, 3):
+            for i_test_size in range(1, 4):
+                for i in range(0, 7):
+                    # Parameters
+                    test_size=0  + i_test_size/10
+                    n_estimators = 50 + i*50
+                    
+                    # Model creation
+                    model = RandomForestRegressor(random_state=0,
+                                                    n_estimators=n_estimators,
+                                                    min_samples_leaf=min_samples_leaf,
+                                                    max_leaf_nodes=max_leaf_nodes)
+                    
+                    # Fitting
+                    data = split_data_for_test(X, y, test_size=test_size)
+                    model.fit(data.train.X, data.train.y)
+                    
+                    # Testing
+                    predictions = model.predict(data.test.X)
+                    acc_total = r2_score(predictions, data.test.y)
+                    
+                    # Debug & Printing
+                    print('----------')
+                    print(f'nº {i} | test_size={test_size} | n_estimators = {n_estimators} | min_samples_leaf = {min_samples_leaf} | max_leaf_nodes={max_leaf_nodes}')
+                    print(f'acc = {acc_total}')
+                    # Cuidado que esse aqui é EM VALORES ABSOLUTOS!
+                    mean_error = mean_squared_error(predictions, data.test.y)
+                    print(f'mean error² = {mean_error}')
+                    pass
