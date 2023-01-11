b3da002ae33944a0e25315374944d9955a551c94
takenoto <mendesrenan5@gmail.com>
2023-01-11 14:36:09 -0300

    removed scaler

--- app/neural_network/nn.py
+++ app/neural_network/nn.py
@@ -21,12 +21,15 @@ def scikit_nn_loop(X, y, debug_print=False):
     param_grid = {
         'mlp_reg__solver': ['adam'],# 'lbfgs', 'sgd', ],
         # TODO como decidir quantas camadas, a qtde em cada uma, etc??
-        'mlp_reg__hidden_layer_sizes': [(10, 5, 10, 5, 10, 5)], # (10,10)],#(40, 60)],# (10, 20), (60, 40), (200, 200)],
+        # ref: https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
+        'mlp_reg__hidden_layer_sizes': [(4, 4, 4, 3)],#(4, 3), (10, 5, 10, 5, 10, 5)], # (10,10)],#(40, 60)],# (10, 20), (60, 40), (200, 200)],
         'mlp_reg__random_state':[1],
         'mlp_reg__max_iter':[2000],
         #'shuffle':[True],
         'mlp_reg__verbose':[False],
-        'mlp_reg__alpha':[0.0001, 1e-5]
+        'mlp_reg__alpha':[0.0001],#, 1e-5]
+        'mlp_reg__activation':['relu', 'tanh',],#'logistic']
+        # 'mlp_reg__tol': [1e-3] #erro 10x mais alto que o padrão
     }
 
 
@@ -35,8 +38,10 @@ def scikit_nn_loop(X, y, debug_print=False):
     # Pq considera mais proporções entre um e outro e os pontos min e máximos
     pipeline = Pipeline(
         [   
+            # Esse scaler tava contribuindo MUITO com o overfitting e com
+            # os dados que ficavam abaixo de 0 (P e S)
             # Com o standard scaler fica melhor, anote lá
-            ('standard_scaler', StandardScaler()), #Minmax fica bem ruim
+            # ('standard_scaler', StandardScaler()), #Minmax fica bem ruim
             # ('robust_scaler', RobustScaler()),
             # ('poly', PolynomialFeatures(degree=3)), # Piorou mto nos meus testes
             ('mlp_reg', reg)
@@ -44,7 +49,7 @@ def scikit_nn_loop(X, y, debug_print=False):
     )
 
 
-    cv = ShuffleSplit(n_splits=10, test_size=0.8, random_state=42)
+    cv = ShuffleSplit(n_splits=3, test_size=0.2, random_state=42) #test_size=0.8
     
 
     grid = GridSearchCV(pipeline,
